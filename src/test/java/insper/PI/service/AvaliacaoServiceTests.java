4e4dcf85b3738a32c40a484c16099a60d89d4e1b
Léo <leomaxmonte14@gmail.com>
2026-09-23 11:16:48 -0300

    testes service

--- src/test/java/insper/PI/service/AvaliacaoServiceTests.java
+++ src/test/java/insper/PI/service/AvaliacaoServiceTests.java
@@ -14,6 +14,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -50,6 +52,49 @@ public class AvaliacaoServiceTests {
         Assertions.assertEquals(5, op.getNota());
         Assertions.assertEquals("23-09-2026", op.getDataAvaliacao());
     }
+
+    @Test
+    public void test_shouldReturnTwoPaymentsWhenListarAvaliacao() {
+        List<Avaliacao> avaliacao = new ArrayList<>();
+        avaliacao.add(new Avaliacao());
+        avaliacao.add(new Avaliacao());
+
+        // cria os mocks
+        Mockito.when(avaliacaoRepository.findAll())
+                .thenReturn(avaliacao);
+
+        // chama o metodo testado
+        List<Avaliacao> response = avaliacaoService.listarAvaliacao();
+
+        // asserts
+        Assertions.assertEquals(2, response.size());
+    }
+
+
+    @Test
+    public void test_shouldCreateAvaliacao() {
+        // mocks
+        SaveAvaliacaoDto dto = new SaveAvaliacaoDto();
+        dto.setNome("PI agil");
+        dto.setConteudo("java");
+        dto.setNota(5);
+        dto.setDataAvaliacao("23-09-2026");
+
+        Avaliacao avaliacao = Avaliacao.toModel(dto);
+
+        Mockito.when(avaliacaoRepository.save(Mockito.any()))
+                .thenReturn(avaliacao);
+
+        // chamada
+        Avaliacao response = avaliacaoService.salvarAvaliacao(dto);
+
+        // asserts
+        Assertions.assertEquals("PI agil", response.getNome());
+        Assertions.assertEquals("java", response.getConteudo());
+        Assertions.assertEquals(5, response.getNota());
+        Assertions.assertEquals("23-09-2026", response.getDataAvaliacao());
+    }
+
 
 
 }
